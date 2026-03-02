d1d865fbbcea6efa6fc5481687cbbddac49a721b
universal143 <rivarora1122@gmail.com>
2026-03-02 22:42:51 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAc9WLJso4c4K-igEpk51nTzTWACfMZTkQ",
+  authDomain: "frndparty-ab2e9.firebaseapp.com",
+  projectId: "frndparty-ab2e9",
+  storageBucket: "frndparty-ab2e9.firebasestorage.app",
+  messagingSenderId: "451894219901",
+  appId: "1:451894219901:web:d161db8f21bc91a9c68d14"
+};
